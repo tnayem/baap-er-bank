6ccbdb368e46017a0c62094e4bda085bca34ab6a
tnayem <tasdidnayem@gmail.com>
2024-01-01 04:06:30 +0600

    Total balance functionlity compleate

--- js/deposit.js
+++ js/deposit.js
@@ -1,10 +1,20 @@
+function addMoney(currentBalance, newBalance){
+    return currentBalance + newBalance;
+}
 document.getElementById('deposit-btn').addEventListener('click',function(){
+    // Deposite Section
     const depositAmount = document.getElementById('deposit-amount')
     const depositValue = Number(depositAmount.value)
 
     const depositTotal = document.getElementById('deposit-total')
     const depositTotalValu = Number(depositTotal.innerText)
-    const totalDeposit = depositTotalValu + depositValue;
+    const totalDeposit = addMoney(depositTotalValu, depositValue)
     depositTotal.innerText = totalDeposit;
+    // Total Balance Section
+    const totalBalanceString = document.getElementById('total-balance')
+    const currentBalance = Number(totalBalanceString.innerText);
+    const totalBalance = addMoney(currentBalance , depositValue)
+    totalBalanceString.innerText = totalBalance;
+    depositAmount.value = '';
     
 })
