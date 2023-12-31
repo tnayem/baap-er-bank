aa46282ea712750462d153185d5bf712a70d5313
tnayem <tasdidnayem@gmail.com>
2024-01-01 04:35:08 +0600

    Withdraw section and total section compleate

--- js/deposit.js
+++ js/deposit.js
@@ -1,7 +1,17 @@
-function addMoney(currentBalance, newBalance){
+function addMoney(currentBalance, newBalance) {
     return currentBalance + newBalance;
 }
-document.getElementById('deposit-btn').addEventListener('click',function(){
+function decressMoney(currentBalance, decressBalance) {
+    if (currentBalance > decressBalance) {
+        return currentBalance - decressBalance;
+    } else {
+        alert("You do not have Sufficient Ballance")
+    }
+
+}
+const totalBalanceString = document.getElementById('total-balance')
+const currentBalance = Number(totalBalanceString.innerText);
+document.getElementById('deposit-btn').addEventListener('click', function () {
     // Deposite Section
     const depositAmount = document.getElementById('deposit-amount')
     const depositValue = Number(depositAmount.value)
@@ -11,10 +21,21 @@ document.getElementById('deposit-btn').addEventListener('click',function(){
     const totalDeposit = addMoney(depositTotalValu, depositValue)
     depositTotal.innerText = totalDeposit;
     // Total Balance Section
-    const totalBalanceString = document.getElementById('total-balance')
-    const currentBalance = Number(totalBalanceString.innerText);
-    const totalBalance = addMoney(currentBalance , depositValue)
+
+    const totalBalance = addMoney(currentBalance, depositValue)
     totalBalanceString.innerText = totalBalance;
     depositAmount.value = '';
-    
+
+})
+// Withdraw Section
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    const withdraw = document.getElementById('total-withdraw')
+    const totalWithdrawAmount = Number(withdraw.innerText);
+
+    const withdrawAmount = document.getElementById('withdraw-amount')
+    const withdrawAmountValu = Number(withdrawAmount.value);
+    const totalWithdraw = addMoney(totalWithdrawAmount, withdrawAmountValu)
+    withdraw.innerText = totalWithdraw;
+    const letBallance = decressMoney(currentBalance, totalWithdraw)
+    totalBalanceString.innerText = letBallance
 })
